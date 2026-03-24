5d6c83fafb9631ab9cb047096d1f9b6f79b7f114
Yasmeen Refay <yasmeenhifny@gmail.com>
2026-03-24 16:07:47 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+ return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind Ready 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App
